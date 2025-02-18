70dd03081b986f91c140e3116d9e5d80f0127381
Michael <michael.picard14@gmail.com>
2025-02-18 18:56:51 +0100

    feat: Ajout de la barre des tâches

---
+++ frontend/src/components/Taskbar/Taskbar.js
@@ -0,0 +1,12 @@
+// src/components/Taskbar/Taskbar.js
+import React from 'react';
+import './Taskbar.css';
+
+const Taskbar = () => {
+    return (
+        <div className='taskbar'>
+            <button>Start</button>
+            <span className='clock'>12:00 PM</span>
+        </div>
+    );
+};
