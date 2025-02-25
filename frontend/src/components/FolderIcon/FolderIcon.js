e68c55b92a03c335f170efa4db92f175b68e3c49
Michael <michael.picard14@gmail.com>
2025-02-25 11:54:26 +0100

    feat: scaling effect

--- frontend/src/components/FolderIcon/FolderIcon.js
+++ frontend/src/components/FolderIcon/FolderIcon.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import './FolderIcon.css';
 
-const FolderIcon = ({ name, onClick  }) => {
+const FolderIcon = ({ name, onClick }) => {
     return (
-        <div className='folder-icon' onClick={onClick}>
-            <img src="assets/components/FolderIcon/Simple/simple-64.png" alt={name} />
-            <span>{name}</span>
-        </div>
+      <div className='folder-icon' onClick={onClick} style={{ transform: 'scale(1.1)' }}>
+        <img src="assets/components/FolderIcon/Simple/simple-64.png" alt={name} />
+        <span>{name}</span>
+      </div>
     );
-};
+  };
 
 export default FolderIcon;
