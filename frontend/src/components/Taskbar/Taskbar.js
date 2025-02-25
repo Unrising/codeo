7a1790e47b578103a772b847bb9081520d208291
Michael <michael.picard14@gmail.com>
2025-02-25 11:51:13 +0100

    feat: Dynamic taskbar

--- frontend/src/components/Taskbar/Taskbar.js
+++ frontend/src/components/Taskbar/Taskbar.js
@@ -2,13 +2,20 @@
 import React from 'react';
 import './Taskbar.css';
 
-const Taskbar = () => {
-    return (
-        <div className='taskbar'>
-            <button>Start</button>
-            <span className='clock'>12:00 PM</span>
-        </div>
-    );
+const Taskbar = ({ openWindows, setActiveWindow }) => {
+  return (
+    <div className="taskbar">
+      <button>Start</button>
+      <div className="taskbar-windows">
+        {openWindows.map((window, index) => (
+          <button key={index} onClick={() => setActiveWindow(window)}>
+            {window}
+          </button>
+        ))}
+      </div>
+      <span className='clock'>{new Date().toLocaleTimeString()}</span>
+    </div>
+  );
 };
 
 export default Taskbar;
